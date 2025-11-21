127249b3cf5bb6e451ad19a55b7e13ab56a357d0
aigars <aigars@inetfactory.com>
2025-11-21 20:16:01 +0200

    broken security

--- src/main/java/com/nasdaq/config/SecurityConfiguration.java
+++ src/main/java/com/nasdaq/config/SecurityConfiguration.java
@@ -25,15 +25,8 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import com.nasdaq.security.jwt.JwtAuthConverter;
-
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import  org.springframework.web.cors.CorsConfiguration;
 import org.springframework.core.annotation.Order;
 @Configuration
 @EnableWebSecurity
@@ -43,38 +36,6 @@ import org.springframework.core.annotation.Order;
 public class SecurityConfiguration  {
 
 
-  private final JwtAuthConverter jwtAuthConverter;
-  
-  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-  private String issuerUri;
-
-  @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-  private String jwkSetUri;
-
-  @Value("${spring.security.oauth2.resourceserver.jwt.client-issuer-uri}")
-  private String clientIssuerUri;
-
-  @Value("${spring.security.oauth2.resourceserver.jwt.client-jwk-set-uri}")
-  private String clientjwkSetUri;
-  
-  
-
-  
-  
-  @Bean
-  @Order(0)
-  public SecurityFilterChain leiFilterChain(HttpSecurity http) throws Exception {
-    
-    return http
-        .securityMatchers(matchers -> matchers
-            .requestMatchers("/**"))
-    .oauth2ResourceServer(oauth2 -> oauth2
-            .authenticationManagerResolver(jwtIssuerAuthenticationManagerResolver()))
-        .sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .csrf(CsrfConfigurer::disable)
-        .build();
-  }
 
   @Bean
   @Order(1)
@@ -106,29 +67,7 @@ public class SecurityConfiguration  {
   }  
     
   
-//https://github.com/spring-projects/spring-security/issues/14677#issuecomment-1978288891
-//https://stackoverflow.com/questions/65398610/extract-authentication-from-bearer-token-after-issuer-resolver-with-spring-secur
-   @Bean
-   public JwtIssuerAuthenticationManagerResolver jwtIssuerAuthenticationManagerResolver() {
-       Map<String , String> issuerToJwkSetUri = new TreeMap<String , String>() {{
-           put(issuerUri,jwkSetUri);
-           put(clientIssuerUri,clientjwkSetUri);
-       }};
-       log.info("issuerUri="+issuerUri);
-       log.info("jwkSetUri="+jwkSetUri);
-       log.info("clientIssuerUri="+clientIssuerUri);
-       log.info("clientjwkSetUri="+clientjwkSetUri);
-       return new JwtIssuerAuthenticationManagerResolver(
-               (issuer) -> {
-                   NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(issuerToJwkSetUri.get(issuer)).build();
-                   jwtDecoder.setJwtValidator(JwtValidators.createDefaultWithIssuer(issuer));
-                   JwtAuthenticationProvider authenticationProvider = new JwtAuthenticationProvider(jwtDecoder);
-                   authenticationProvider.setJwtAuthenticationConverter(jwtAuthConverter);
-                   return authenticationProvider::authenticate;
-               }
-       );
-   }
-  
+   
   
 }
   
